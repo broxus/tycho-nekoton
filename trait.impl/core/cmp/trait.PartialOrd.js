109f97a24f42775c151658141ee871ca63cbedc5
MrWad3r <MrWad3r@users.noreply.github.com>
2025-07-16 12:40:47 +0000

    deploy: 60faf969afb88336b19b246d3c76a4f29668cda9

--- trait.impl/core/cmp/trait.PartialOrd.js
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["nekoton_utils",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"nekoton_utils/time/struct.Timings.html\" title=\"struct nekoton_utils::time::Timings\">Timings</a>"]]]]);
+    var implementors = Object.fromEntries([["nekoton_utils",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"nekoton_utils/time/struct.Timings.html\" title=\"struct nekoton_utils::time::Timings\">Timings</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
