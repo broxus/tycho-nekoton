3cd45234081962f459644ff1aa729c7e993510f6
Stanislav Eliseev <s.eliseev@dexpa.io>
2026-05-28 14:31:44 +0200

    Update trace transaction to process in-out messages + tests

--- core/src/transactions/mod.rs
+++ core/src/transactions/mod.rs
@@ -1,7 +1,10 @@
 mod trace_transaction;
 mod transaction_tree;
 
-pub use trace_transaction::TraceTransaction;
+pub use trace_transaction::{
+    TraceMessage, TraceTransaction, TraceTransactionError, TraceTransactionEvent,
+    TraceTransactionPolling, TraceTransactionResult, TraceTransactionStep,
+};
 pub use transaction_tree::{
     TransactionTreeError, TransactionTreeMessage, TransactionTreeResult, TransactionTreeStep,
     TransactionTreeStream, TransactionsTreeStream,
